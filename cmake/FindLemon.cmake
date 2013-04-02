2ee0feca2003f8532228b1629e93ab36fc24b6b1
KokaKiwi <admin@kokaelkiwi.eu>
2013-04-02 22:03:49 +0200

    Modify FindLemon.cmake

--- cmake/FindLemon.cmake
+++ cmake/FindLemon.cmake
@@ -20,36 +20,36 @@ if (LEMON)
         LEMON_VERSION ${LEMON_VERSION_FULL}
     )
 
-    function(lemon_generate target target_ext source_file)
+    function(lemon_generate target target_dir target_ext source_file)
         get_filename_component(fext "${source_file}" EXT)
         get_filename_component(fpath "${source_file}" PATH)
         get_filename_component(fbase "${source_file}" NAME_WE)
 
-        set("${target}_OUT" "${fpath}/${fbase}.out" PARENT_SCOPE)
-        set("${target}_HEADER" "${fpath}/${fbase}.h" PARENT_SCOPE)
-        set("${target}_SOURCE" "${fpath}/${fbase}${target_ext}" PARENT_SCOPE)
+        set("${target}_OUT" "${target_dir}/${fbase}.out" PARENT_SCOPE)
+        set("${target}_HEADER" "${target_dir}/${fbase}.h" PARENT_SCOPE)
+        set("${target}_SOURCE" "${target_dir}/${fbase}${target_ext}" PARENT_SCOPE)
 
-        if(NOT EXISTS "${fpath}/")
-            set(LEMON_MKDIR COMMAND ${CMAKE_COMMAND} -E make_directory "${fpath}")
-        endif(NOT EXISTS "${fpath}/")
+        if(NOT EXISTS "${target_dir}/")
+            set(LEMON_MKDIR COMMAND ${CMAKE_COMMAND} -E make_directory "${target_dir}")
+        endif(NOT EXISTS "${target_dir}/")
 
         add_custom_command(
-            OUTPUT "${fpath}/${fbase}${target_ext}"
-            OUTPUT "${fpath}/${fbase}.out"
-            OUTPUT "${fpath}/${fbase}.h"
+            OUTPUT "${target_dir}/${fbase}${target_ext}"
+            OUTPUT "${target_dir}/${fbase}.out"
+            OUTPUT "${target_dir}/${fbase}.h"
             ${LEMON_MKDIR}
             COMMAND ${LEMON}
                 ARGS    ${LEMON_FLAGS}
                         "${CMAKE_CURRENT_SOURCE_DIR}/${source_file}"
             COMMAND mv
                 ARGS    "${CMAKE_CURRENT_SOURCE_DIR}/${fpath}/${fbase}.out"
-                        "${fpath}/${fbase}.out"
+                        "${target_dir}/${fbase}.out"
             COMMAND mv
                 ARGS    "${CMAKE_CURRENT_SOURCE_DIR}/${fpath}/${fbase}.h"
-                        "${fpath}/${fbase}.h"
+                        "${target_dir}/${fbase}.h"
             COMMAND mv
                 ARGS    "${CMAKE_CURRENT_SOURCE_DIR}/${fpath}/${fbase}.c"
-                        "${fpath}/${fbase}${target_ext}"
+                        "${target_dir}/${fbase}${target_ext}"
             DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${source_file}"
             COMMENT "Generating ${target}"
         )

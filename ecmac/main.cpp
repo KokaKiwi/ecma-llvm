2c1e3ef52a57eb2210c804c00b357008b201cbd5
KokaKiwi <kokakiwi@kokakiwi.net>
2013-12-01 20:52:01 +0100

    Module name for stdin source is now "unnamed" instead of "*stream*"

--- ecmac/main.cpp
+++ ecmac/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
         if (argc == 1)
         {
             program = toolchain::Source::fromStream(std::cin).parse(DEBUG);
-            moduleName = "*stream*";
+            moduleName = "unnamed";
         }
         else if (argc == 2)
         {

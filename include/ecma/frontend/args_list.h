a6b016ae494ae24e5ffe78331cdc3d20c9d6939a
KokaKiwi <kokakiwi@kokakiwi.net>
2014-01-05 18:46:04 +0100

    - Modify args list

--- include/ecma/frontend/args_list.h
+++ include/ecma/frontend/args_list.h
@@ -7,10 +7,11 @@ ECMA_ARG(llvm::cl::opt<bool>, parseOnly, "P", llvm::cl::desc("Run the parse phas
 
 // Compile
 ECMA_ARG(llvm::cl::opt<bool>, compileOnly, "S", llvm::cl::desc("Compile only; do not assemble or link"));
-ECMA_ARG(llvm::cl::opt<bool>, llvmOutput, "emit-llvm", llvm::cl::desc("Produce an LLVM assembly file if used with -S option;\nproduce an LLVM bitcode file otherwise"));
+ECMA_ARG(llvm::cl::opt<bool>, llvmOutput, "emit-llvm", llvm::cl::desc("Produce an LLVM assembly file if used with -S option; produce an LLVM bitcode file otherwise"));
 
 // Output
 ECMA_ARG(llvm::cl::opt<std::string>, output, "o", llvm::cl::desc("Write output to <filename>"), llvm::cl::value_desc("filename"));
+ECMA_ARG(llvm::cl::alias, outputAlias, "output", llvm::cl::desc("Alias for -o"), llvm::cl::aliasopt(output));
 
 // Common
 ECMA_ARG(llvm::cl::list<std::string>, compilerFlags, "Z", llvm::cl::desc("Internal compiler flags."));

ba3b461222da2cc7c465598ab8f4e2c3b2bdcdcc
KokaKiwi <kokakiwi@kokakiwi.net>
2014-01-06 01:23:34 +0100

    - Add a category for ecmac-specific command line options.

--- include/ecma/frontend/args_list.h
+++ include/ecma/frontend/args_list.h
@@ -1,18 +1,21 @@
 
+// Category
+ECMA_ARG(llvm::cl::OptionCategory, ecmaOptions, "ecmac options");
+
 // Input
-ECMA_ARG(llvm::cl::list<std::string>, input, llvm::cl::Positional, llvm::cl::desc("<input files>"));
+ECMA_ARG(llvm::cl::list<std::string>, input, llvm::cl::Positional, llvm::cl::desc("<input files>"), llvm::cl::cat(ecmaOptions));
 
 // Parse
-ECMA_ARG(llvm::cl::opt<bool>, parseOnly, "P", llvm::cl::desc("Run the parse phase."));
+ECMA_ARG(llvm::cl::opt<bool>, parseOnly, "P", llvm::cl::desc("Run the parse phase."), llvm::cl::cat(ecmaOptions));
 
 // Compile
-ECMA_ARG(llvm::cl::opt<bool>, compileOnly, "S", llvm::cl::desc("Compile only; do not assemble or link"));
-ECMA_ARG(llvm::cl::opt<bool>, llvmOutput, "emit-llvm", llvm::cl::desc("Produce an LLVM assembly file if used with -S option; produce an LLVM bitcode file otherwise"));
+ECMA_ARG(llvm::cl::opt<bool>, compileOnly, "S", llvm::cl::desc("Compile only; do not assemble or link"), llvm::cl::cat(ecmaOptions));
+ECMA_ARG(llvm::cl::opt<bool>, llvmOutput, "emit-llvm", llvm::cl::desc("Produce an LLVM assembly file if used with -S option; produce an LLVM bitcode file otherwise"), llvm::cl::cat(ecmaOptions));
 
 // Output
-ECMA_ARG(llvm::cl::opt<std::string>, output, "o", llvm::cl::desc("Write output to <filename>"), llvm::cl::value_desc("filename"));
-ECMA_ARG(llvm::cl::alias, outputAlias, "output", llvm::cl::desc("Alias for -o"), llvm::cl::aliasopt(output));
+ECMA_ARG(llvm::cl::opt<std::string>, output, "o", llvm::cl::desc("Write output to <filename>"), llvm::cl::value_desc("filename"), llvm::cl::cat(ecmaOptions));
+ECMA_ARG(llvm::cl::alias, outputAlias, "output", llvm::cl::desc("Alias for -o"), llvm::cl::aliasopt(output), llvm::cl::cat(ecmaOptions));
 
 // Common
-ECMA_ARG(llvm::cl::list<std::string>, compilerFlags, "Z", llvm::cl::desc("Internal compiler flags."));
-ECMA_ARG(llvm::cl::opt<bool>, noColor, "no-color", llvm::cl::desc("Disable colored output."));
+ECMA_ARG(llvm::cl::list<std::string>, compilerFlags, "Z", llvm::cl::desc("Internal compiler flags."), llvm::cl::cat(ecmaOptions));
+ECMA_ARG(llvm::cl::opt<bool>, noColor, "no-color", llvm::cl::desc("Disable colored output."), llvm::cl::cat(ecmaOptions));

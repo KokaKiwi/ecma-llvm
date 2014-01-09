ae16ee8158f09c9094e0f2d762368b3f34f46395
KokaKiwi <kokakiwi@kokakiwi.net>
2014-01-09 10:42:52 +0100

    - Add line to 'native.js' test.

--- tests/parser/success/native.js
+++ tests/parser/success/native.js
@@ -4,3 +4,5 @@ var strdup = native function strdup(s: *u8): *u8;
 
 var a: int = 25;
 var str: string = "Hello world!";
+var a: u8 = 'a', b: uint = 25, c: u32 = 0x01234567;
+var ptr: *void = memcpy;

6475d6e41aa82778a4c21ad4bb6e5aaef03822c4
KokaKiwi <kokakiwi@kokakiwi.net>
2014-01-13 01:46:33 +0100

    - Update stats.

--- tools/stats.sh
+++ tools/stats.sh
@@ -3,7 +3,7 @@ HERE=$(dirname $0)
 PROJECT_ROOT=$(dirname ${HERE})
 
 cd ${PROJECT_ROOT}
-cloc --exclude-dir=build --not-match-d='/\..*/' --out=STATS --quiet --progress-rate=0 .
+cloc --exclude-dir=build --not-match-d='\..*' --out=STATS .
 echo >> STATS
 
 echo "Contributors (git):" >> STATS
